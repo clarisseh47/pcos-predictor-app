dad499b8c40c9ae89519acb6e15e4c1a6054a7e3
c30ra1 <risha.agarwal23@gmail.com>
2025-04-03 21:31:21 +0000

    add in screening questions

--- screening.py
+++ screening.py
@@ -1,4 +1,27 @@
 import streamlit as st
 def display_page():    
     st.title('Screening Quiz')
-    BMI = st.radio('Is your BMI value...?',["Underweight", "Normal", "Overweight", "Obese"])
+    height = st.number_input('How tall are you (in inches)?',min_value=1, max_value=100)
+    weight = st.number_input ('How much do you weight (in pounds)?',min_value=1,max_value=1000)
+    BMI = (weight/(height**2))*703
+    BMI_input = 0.0
+    if BMI<18.5:
+        BMI_input=0.0
+    elif BMI<25:
+        BMI_input = 1.0
+    elif BMI<30:
+        BMI_input = 2.0
+    else:
+        BMI_input = 3.0
+    MR = st.radio('Is your period regular or irregular?',['regular','irregular'])
+    MR_input = 0.0
+    if MR == "irregular":
+        MR_input = 1.0
+    hirsutism = st.radio('Do you have hirsutism? Hirsutism is excessive hair growth.',['No','Yes'])
+    hirsutism_input = 0.0 
+    if hirsutism == "Yes":
+        hirsutism_input = 1.0
+    
+    
+
+
