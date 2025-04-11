41807cf510ea92832c643370e063dd60e6ac393e
c30ra1 <risha.agarwal23@gmail.com>
2025-04-11 23:35:40 +0000

    addded in more screen questions

--- screening.py
+++ screening.py
@@ -22,13 +22,59 @@ def display_page():
     if hirsutism == "Yes":
         hirsutism_input = 1.0
     AcneSeverity = st.radio('How would you describe your acne severity?',["No acne", "Mild", "Moderate", "Severe"])
+    AcneSeverity_input = 0.0
+    if AcneSeverity == "No acne":
+        AcneSeverity_input = 0.0
+    elif AcneSeverity == "Mild":
+        AcneSeverity_input = 1.0
+    elif AcneSeverity == "Moderate":
+        AcneSeverity_input = 2.0
+    else: 
+        AcneSeverity_input = 3.0
     FamilyHistoryofPCOS = st.radio('Have any of your blood relatives been diagnosed with PCOS before?',["No", "Yes"])
+    FamilyHistoryofPCOS_input = 0.0
+    if FamilyHistoryofPCOS == "Yes":
+        FamilyHistoryofPCOS_input = 1.0
     InsulinResistance = st.radio('Do you have insluin resistance?',["No", "Yes"])
+    InsulinResistance_input = 0.0
+    if InsulinResistance == "Yes":
+        InsulinResistance_input = 1.0
     StressLevels = st.radio('What is your stresslevel?',["Low","Medium","High"])
+    StressLevels_input = 0.0
+    if StressLevels == "Low":
+        StressLevels_input = 0.0
+    elif StressLEvels == "Medium":
+        StressLevels_input = 1.0
+    else: 
+        StressLevels_input = 2.0
     UrbanRural = st.radio('Do you live in a urban or rural area?',["Urban","Rural"])
+    UrbanRural_input = 0.0
+    if UrbanRural == "Rural":
+        UrbanRural_input = 1.0
     SocioeconomicStatus = st.radio('What is your socioeconomic status?',["Low","Middle","High"])
+    SS_input = 0.0
+    if SocioeconomicStatus == "Low":
+        SS_input = 0.0
+    elif SocioeconomicStatus == "Medium":
+        SS_input = 1.0
+    else:
+        SS_input = 2.0
     FertilityConcerns = st.radio('Do you have fertility concern?',["No","Yes"])
+    FertilityConcerns_input = 0.0
+    if FertilityConcerns == "Yes"
+        FertilityConcerns_input = 1.0
     Ethnicity = st.radio('What is your ethnicity?',["African", "Asian", "Caucasian", "Hispanic", "Other"])
+    Ethnicity_input = 0.0
+    if Ethnicity == "African":
+        Ethnicity_input == 0.0
+    elif Ethnicity == "Asian":
+        Ethnicity_input == 1.0
+    elif Ethnicity == "Caucasian":
+        Ethnicity_input == 2.0
+    elif Ethnicity == "Hispanic":
+        Ethnicity_input == 3.0
+    else:
+        Ethnicity_input == 4.0
     
 
 
