43cfe85425347fed02a2446114d57f4fefc7aead
clarisseh47 <122301567+clarisseh47@users.noreply.github.com>
2025-04-24 21:52:55 +0000

    added content to home and background pages, renamed images, modified devcontainer

--- background.py
+++ background.py
@@ -1,3 +1,5 @@
 import streamlit as st
 def display_page():
-    st.title("PCOS Background") 
+    col1, col2 = st.columns(2)
+    col1.write('background')
+    col2.image('page2.webp', caption='Source: lyndhurstgyn.com', use_container_width=True)
--- home.py
+++ home.py
@@ -6,17 +6,20 @@ import about_us
 
 def display_home():
     col1, col2 = st.columns(2)
-    col1.write("This is column 1")
-    col2.write("This is column 2")
+    col1.image('PCOSense_logo.png', use_container_width=True)
+    col1.write('Disclaimer: This information is provided for educational purposes only '
+    'and is not a substitute for professional medical advice, diagnosis, or treatment. ' \
+    'Always consult with a qualified healthcare provider regarding any questions you may ' \
+    'have about your health or before making any decisions related to your medical condition.')
+    
+    col2a, col2b = col2.columns(2)
+    url = 'https://stock.adobe.com/search?k=pcos'
+    col2a.image('page1_left.jpg', caption='[Image Source](%s)' % url, use_container_width=True)
+    col2b.image('page1_right.jpg', caption='[Image Source](%s)' % url, use_container_width=True)
+    col2.write('We want to bring awareness to Polycystic Ovary Syndrome (PCOS). ' \
+    'Through this web application, users can take a screening quiz, which uses a machine ' \
+    'learning model to predict if an individual may have PCOS. ')
 
-
-    # st.title("PCOS Predictor")
-    # st.write(
-    #     "Here is our PCOS predictor web app!"
-    # )
-    # st.write(
-    #     "By Risha and Reha"
-    # )
 
 st.sidebar.title("Menu")
 option = st.sidebar.selectbox("select a page",['Home',"Background","Screening Quiz","Treatments + Resources","About Us"])
